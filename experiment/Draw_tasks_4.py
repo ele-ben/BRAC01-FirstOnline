8d426efe308237f2f09e068e692d4fcf22288167
unknown <elena.ben95@gmail.com>
2020-04-03 15:08:49 +0200

    the script correctly prodeuces 8 instructions spreadsheets for the counterbalancing

--- experiment/Draw_tasks_4.py
+++ experiment/Draw_tasks_4.py
@@ -118,7 +118,8 @@ for framecolor in ["blue", "red"]: # I don't need the black here
 # save the df in a csv to be uploaded in Gorilla
 blackCuDf.to_csv(imgSheetName + ".csv", sep = ";")
 
-
+# create the 8 different instructions spreadsheets
+# define the strings composing each instruction
 magn =  "greater or less than 5."
 par = "odd or even."
 horiMap = "When the rectangle is horizontal, your task is to tell whether the number is "
@@ -136,9 +137,9 @@ keys1 = [[A, L], [L, A]]
 for task in tasks:
     for key in keys:
         for key1 in keys1:
-            instr = pd.DataFrame([], columns = ["display", "horiMap", "verMap", "greatM", "lessM", "oddM", "evenM"])
+            instr = pd.DataFrame([], columns = ["display", "horiMap", "verMap", "greatMap", "lessMap", "oddMap", "evenMap"])
             row = {"display": "Instructions", "horiMap": horiMap + task[0],
-                "verMap": verMap + task[1], "greatM": key[0] + greatM, "lessM": key[1]+ lessM, "oddM": key1[0] + oddM, "evenM": key1[1] + evenM}
+                "verMap": verMap + task[1], "greatMap": key[0] + greatM, "lessMap": key[1]+ lessM, "oddMap": key1[0] + oddM, "evenMap": key1[1] + evenM}
             instr = instr.append(row, ignore_index = True)
             instFile = task[0][:3] + key[0][6:7] + key1[0][6:7]
             instr.to_csv("spreadsheets/"+ instFile + ".csv", sep = ";", index= False)
